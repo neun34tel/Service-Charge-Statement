95c5b45c9487bed8b4da0bf018c2ea0ddb0ba709
Kay Peikert <1233584+neun34tel@users.noreply.github.com>
2025-05-20 03:50:26 +0200

    Description for Housing Uni mandatory
    - necessary, it is used in vital parts

--- app/models/building.rb
+++ app/models/building.rb
@@ -4,10 +4,8 @@ class Building < ApplicationRecord
   validates :name,
             presence: true,
             length: { minimum: 3, maximum: 100 }
-
   validates :street, :house_number, :zip_code, :city, :country,
             presence: true
-
   validates :zip_code,
             format: { with: /\A\d{5}\z/, message: "muss 5-stellig sein" },
             if: -> { country == 'Germany' }
--- app/models/housing_unit.rb
+++ app/models/housing_unit.rb
@@ -4,6 +4,6 @@ class HousingUnit < ApplicationRecord
   # Validierungen hinzufügen
   validates :residential_area, :usable_area, :total_area_share,
             numericality: { greater_than_or_equal_to: 0 }
+  validates :description, length: { maximum: 500 }, allow_blank: false
 
-  validates :description, length: { maximum: 500 }, allow_blank: true
 end
